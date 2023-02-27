01e34545c5979971244f2e17cf553a540bd565b3
Adin Sila6 <asilajdzic2000@gmail.com>
2023-02-27 18:08:27 +0100

    Added shop, cart context, cart dropdown

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
+
 import App from "./App";
 import "./index.scss";
 
@@ -11,7 +14,11 @@ root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<UserProvider>
-				<App />
+				<ProductsProvider>
+					<CartProvider>
+						<App />
+					</CartProvider>
+				</ProductsProvider>
 			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
