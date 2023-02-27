880d7f91d12dce124583d1ea60075ea10779b518
Adin Sila6 <asilajdzic2000@gmail.com>
2023-02-27 13:38:32 +0100

    Centralised sign in/out

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,12 +9,7 @@ import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
-	const { currentUser, setCurrentUser } = useContext(UserContext);
-	const signOutHandler = async () => {
-		await signOutUser();
-		setCurrentUser(null);
-	};
-
+	const { currentUser } = useContext(UserContext);
 	return (
 		<Fragment>
 			<div className="navigation">
@@ -26,7 +21,7 @@ const Navigation = () => {
 						SHOP
 					</Link>
 					{currentUser ? (
-						<span className="nav-link" onClick={signOutHandler}>
+						<span className="nav-link" onClick={signOutUser}>
 							SIGN OUT
 						</span>
 					) : (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,7 @@ import {
 	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
 	signOut,
+	onAuthStateChanged,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -19,6 +20,7 @@ const firebaseConfig = {
 	appId: "1:725566952123:web:3ccfef935d733ae9644f9e",
 };
 
+// eslint-disable-next-line
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
@@ -77,3 +79,6 @@ export const signInUserWithEmailAndPassword = async (email, password) => {
 };
 
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+	onAuthStateChanged(auth, callback);
