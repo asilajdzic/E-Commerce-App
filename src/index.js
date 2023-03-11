3c1c06e8ccc44de466f72ba507be1f4623dd8101
asilajdzic <125396448+asilajdzic@users.noreply.github.com>
2023-03-11 22:59:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,8 +20,7 @@ root.render(
 				</BrowserRouter>
 			</PersistGate>
 		</Provider>
-	</React.StrictMode>,
-	root
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
